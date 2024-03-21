44bc1b86025968f2fe7beaf4bdc24e26444955e3
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2024-03-21 09:54:32 +0700

    Buat kode menjadi lebih efisien dan mudah dibaca

--- helper.go
+++ helper.go
@@ -11,32 +11,19 @@ func roundFloat(val float64, precision uint) float64 {
 }
 
 func PickAWinner(team1 Team, team2 Team) {
+	var winner Team
+	var isWinnerScoreMoreThan100 bool
+	var isDraw bool
 
 	if team1.Avg > team2.Avg {
-		fmt.Printf("Tim %s menjadi pemenang karena memiliki skor lebih tinggi dari tim lawan\n", team1.Name)
-		var isScoreMoreThan100 bool
-		for _, score := range team1.Scores {
-			if score > 100 {
-				isScoreMoreThan100 = true
-				break
-			}
-		}
-		if isScoreMoreThan100 {
-			fmt.Println("dan memiliki skore lebih dari 100")
-		}
+		winner = team1
 	} else if team1.Avg < team2.Avg {
-		fmt.Printf("Tim %s menjadi pemenang karena memiliki skor lebih tinggi dari tim lawan\n", team2.Name)
-		var isScoreMoreThan100 bool
-		for _, score := range team2.Scores {
-			if score > 100 {
-				isScoreMoreThan100 = true
-				break
-			}
-		}
-		if isScoreMoreThan100 {
-			fmt.Println("dan memiliki skore lebih dari 100")
-		}
+		winner = team2
 	} else {
+		isDraw = true
+	}
+
+	if isDraw {
 		var isTeam1ScoreMoreThan100 bool
 		var isTeam2ScoreMoreThan100 bool
 
@@ -59,6 +46,17 @@ func PickAWinner(team1 Team, team2 Team) {
 		} else {
 			fmt.Println("Hasil seri, kedua tim memiliki skor kurang dari 100")
 			fmt.Println("Jadi tidak ada yang mendapat trofi")
+		}
+	} else {
+		fmt.Printf("Tim %s menjadi pemenang karena memiliki skor lebih tinggi dari tim lawan\n", winner.Name)
+		for _, score := range winner.Scores {
+			if score > 100 {
+				isWinnerScoreMoreThan100 = true
+				break
+			}
+		}
+		if isWinnerScoreMoreThan100 {
+			fmt.Println("dan memiliki skore lebih dari 100")
 		}
 	}
 }
